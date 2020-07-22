e8fcc1be6e2e28e8fb10e0154db4e91ec874ad9d
Aaron Montgomery <montaaron11@gmail.com>
2020-07-22 05:19:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def start_game(player1, player2)
     binding.pry
-  puts "Hello #{player1} & #{player2}"
+  print "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1, player2)
