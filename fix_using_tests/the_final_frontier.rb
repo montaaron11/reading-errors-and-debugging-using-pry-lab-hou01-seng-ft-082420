9217dfc4f758a178eb002d6b32d5507e04b343e7
Aaron Montgomery <montaaron11@gmail.com>
2020-07-22 06:33:03 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,12 +1,12 @@
 # don't forget to add: require 'pry'
 require 'pry'
-def star_date
+def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
 
-def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+def state_log(generate_star_date)
+  "Captain's Log, star date #{generate_star_date}."
 end
 
 def engage
