9712fb50deb704ed2a15f5e737ae704241616a70
Aaron Montgomery <montaaron11@gmail.com>
2020-07-22 05:58:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,10 @@
-
+# don't forget to add: require 'pry'
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     
-   10.times do 
-     print "s" + string
+    10.times "s" + string
+  else
     string
   end
+end
