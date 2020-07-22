d416785cac9acc4d5ce2202e9ce28f732e71f1f1
Aaron Montgomery <montaaron11@gmail.com>
2020-07-22 06:14:13 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
 # don't forget to add: require 'pry'
 require 'pry'
-def snake_it_up(string)
+def snake_it_up(string[1])
   if string[0] == "s"
-    10 * string[0] + string
+    binding.pry
+    (10 * string[0].to_i+1) + string[1]
   else
     string
   end
